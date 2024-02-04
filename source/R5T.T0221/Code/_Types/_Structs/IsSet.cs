6ead46be458adab0efcf379d0ba542e38fde40ec
davidcoats <david@getsafetycone.com>
2024-02-03 18:24:11 -0800

    Interim changes.

---
+++ source/R5T.T0221/Code/_Types/_Structs/IsSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.T0221
+{
+    /// <summary>
+    /// Instances are used to communicate that certain values have been set.
+    /// </summary>
+    /// <typeparam name="T">A dummy type that communicates what is set.</typeparam>
+    public struct IsSet<T>
+    {
+        // No properties.
+    }
+}
