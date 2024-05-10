b14433659a228108bac51fd0b6e7ff9181ff2066
davidcoats <david@getsafetycone.com>
2024-05-10 00:07:32 -0700

    Interim changes.

---
+++ source/R5T.T0221/Code/_Types/_Structs/EnsuredToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using R5T.T0142;
+
+
+namespace R5T.T0221
+{
+    /// <summary>
+    /// A struct (so that no instance actually has to be created for out parameters), with a dummy type parameter (meant to specify the output type of the function),
+    /// meant for use as an "out" input argument to a function that facilitates communication of whether a function ensures some result internally (or if that result
+    /// must be ensured externally).
+    /// <para>You can use the name of the function parameter to communicate the ensurance afforded by the function.</para>
+    /// </summary>
+    /// <remarks>
+    /// You often want to ensure some end result (for example, that values have been ordered alphabetically).
+    /// The question is, for a function caller, do you have to ensure that result? Or does the function already internally ensure that result?
+    /// If the function has an "out" parameter communicating the ensured result, then the function ensures the result.
+    /// <para>
+    /// Generally callers can omit the type name and variable name of the ensured token (use the "_" discard keyword).
+    /// This is because the ensurance token is only meant to be consumed from the intellisense of a method.
+    /// </para>
+    /// </remarks>
+    [UtilityTypeMarker]
+    public struct EnsuredToken
+    {
+    }
+
+    /// <inheritdoc cref="EnsuredToken"/>
+    /// <typeparam name="T">Dummy type parameter, meant to specify the ensurance type.</typeparam>
+    [UtilityTypeMarker]
+    public struct EnsuredToken<T>
+    {
+    }
+}
--- source/R5T.T0221/Code/_Types/_Structs/OverloadToken.cs
+++ source/R5T.T0221/Code/_Types/_Structs/OverloadToken.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 
+using R5T.T0142;
+
 
 namespace R5T.T0221
 {
     /// <summary>
     /// A struct (so that no instance actually has to be created for out parameters), with a dummy type parameter (meant to specify the output type of the function),
     /// meant for use as an "out" input argument to a function allowing for easy function overload specification.
+    /// <para>
+    /// NOTE: if a function overload provides an overload token, the caller should <string>always</string> take care to specify the type (instead of using the "var" keyword)
+    /// even if there is currently only one function overload with an overload token.
+    /// This is because there might be more in the future, and if someone adds another in the future, all your call sites will now be ambiguous!
+    /// </para>
+    /// However, generally the variable name of the overload token can be omitted (use the "_" discard variable name).
     /// </summary>
     /// <remarks>
     /// Since C# does not differentiate function overloads based on output parameter type, only input parameter types,
     /// well make an input parameter that specifies the output type so that function overloads are still differentiated.
     /// </remarks>
     /// <typeparam name="T">Dummy type parameter, meant to specify the output type of a function in overloads.</typeparam>
-    public struct Overload<T>
+    [UtilityTypeMarker]
+    public struct OverloadToken<T>
     {
     }
 }
