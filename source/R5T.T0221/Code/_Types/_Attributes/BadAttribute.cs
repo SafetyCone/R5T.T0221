8862de5c564407887a1c71b73f8951a086e6e2d1
davidcoats <david@getsafetycone.com>
2025-07-25 12:25:49 -0700

    Interim commit.

--- source/R5T.T0221/Code/_Types/_Attributes/BadAttribute.cs
+++ source/R5T.T0221/Code/_Types/_Attributes/BadAttribute.cs
@@ -9,7 +9,7 @@ namespace R5T.T0221
     /// Similar to the <see cref="ObsoleteAttribute"/>, but indicates that the application element is bad in some way.
     /// </summary>
     /// <remarks>
-    /// Allowed on all application elements.
+    /// Allowed on all code elements.
     /// Multiple are allowed (although discouraged).
     /// Inherited is false; just because the base class is bad doesn't mean all derived classes are.
     /// </remarks>
